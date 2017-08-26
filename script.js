5fb0ccb016fa7403d7a9660866657b0791235ac6
John Potts <john@arlingtonchurch.com>
2017-08-26 01:27:21 -0400

    Got it working!

--- script.js
+++ script.js
@@ -1,61 +1,86 @@
 
 
 $( document ).ready(function() {
-  var winningCombinations=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]],
- xTiles=[], oTiles=[], tile1Checked=null, tile2Checked=null,tile3Checked=null,tile4Checked=null,
- tile5Checked=null,tile6Checked=null,tile7Checked=null,tile8Checked=null,tile9Checked=null;
+  var player1=null, player2=null, winningCombinations=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]],
+ xTiles=[], oTiles=[], tilesClicked=[], playerTurn=null;
 
- $("#board").click(function () {
-     $(".game-tile").slideToggle("slow");
+$('body').on('click', '.x', function (){playerTurn="X"; player1="X";
+$("#board").empty();
+ $("#board").append('<div class ="choose-opponent"><br />Choose your <br />opponent <br /><br/></br/><span class="cpu">CPU</span><span class ="human">Human</span></div>');
+});
+$('body').on('click', '.o', function (){playerTurn="O"; player1="O";
+$("#board").empty();
+ $("#board").append('<div class ="choose-opponent"><br />Choose your <br />opponent <br /><br/></br/><span class="cpu">CPU</span><span class ="human">Human</span></div>');
+});
+  $('body').on('click', '.CPU', function (){ player2="CPU";
+   $("#board").empty();
+     $("#board").append('<div class="game-tile" id="tile-1"></div><div class="game-tile" id="tile-2"></div><div class="game-tile" id="tile-3"></div><div class="game-tile" id="tile-4"></div><div class="game-tile" id="tile-5"></div><div class="game-tile" id="tile-6"></div><div class="game-tile" id="tile-7"></div><div class="game-tile" id="tile-8"></div><div class="game-tile" id="tile-9"></div>');
  });
-//player turn- click tiles
-/*
-$("#tile-1").click(function(){
-if(!tile1Checked){
-  $("#tile-1").append("X");
-xTiles.push(1);
-tile1Checked=1;
-didTheyWin(xTiles);
+ $('body').on('click', '.human', function (){
+  $("#board").empty();
+    $("#board").append('<div class="game-tile" id="tile-1"></div><div class="game-tile" id="tile-2"></div><div class="game-tile" id="tile-3"></div><div class="game-tile" id="tile-4"></div><div class="game-tile" id="tile-5"></div><div class="game-tile" id="tile-6"></div><div class="game-tile" id="tile-7"></div><div class="game-tile" id="tile-8"></div><div class="game-tile" id="tile-9"></div>');
+});
+
+$('body').on('click', '#tile-1', function(){console.log("1");tileClickFunction(1);});
+$('body').on('click', '#tile-2', function(){tileClickFunction(2);});
+$('body').on('click', '#tile-3', function(){tileClickFunction(3);});
+$('body').on('click', '#tile-4', function(){tileClickFunction(4);});
+$('body').on('click', '#tile-5', function(){tileClickFunction(5);});
+$('body').on('click', '#tile-6', function(){tileClickFunction(6);});
+$('body').on('click', '#tile-7', function(){tileClickFunction(7);});
+$('body').on('click', '#tile-8', function(){tileClickFunction(8);});
+$('body').on('click', '#tile-9', function(){tileClickFunction(9);});
+
+var tileClickFunction = function(tileNumber){
+if(tilesClicked.indexOf(tileNumber)==-1){ //make sure its not checked
+  if (playerTurn == "X"){
+    $("#tile-"+tileNumber).append("X");  //check it
+tilesClicked.push(tileNumber); //mark it
+    xTiles.push(tileNumber);   //add to playerTiles
+didTheyWin(xTiles);  //check for win/tie
+if(player2 == "CPU" && player1 == "X"){cpuTurn("O");}
+else{  playerTurn ="O";}   //next player turn
+                   }
+else{
+        $("#tile-"+tileNumber).append("O");  //check it
+  tilesClicked.push(tileNumber); //mark it
+      oTiles.push(tileNumber);   //add to playerTiles
+  didTheyWin(oTiles);  //check for win/tie
+  if(player2 == "CPU" && player1 == "O"){cpuTurn("X");}
+  else{playerTurn ="X";}   //next player turn
 }
-
-});
-$("#tile-2").click(function(){
-$("#tile-2").append("X");
-});
-$("#tile-3").click(function(){
-$("#tile-3").append("X");
-});
-$("#tile-4").click(function(){
-$("#tile-4").append("X");
-});
-$("#tile-5").click(function(){
-$("#tile-5").append("X");
-});
-$("#tile-6").click(function(){
-$("#tile-6").append("X");
-});
-$("#tile-7").click(function(){
-$("#tile-7").append("X");
-});
-$("#tile-8").click(function(){
-$("#tile-8").append("X");
-});
-$("#tile-9").click(function(){
-$("#tile-9").append("X");
-});
+}
+};
 
 
-function didTheyWin(arg){
+
+var didTheyWin = function(playerTiles){
   for (i=0;i<winningCombinations.length;i++){
-    if (arg.indexOf(winningCombinations[i][0]) >= 0 &&
-   arg.indexOf(winningCombinations[i][1]) >= 0 &&
-   arg.indexOf(winningCombinations[i][2]) >= 0){
-      alert ("you win!");
-    }
-    }
-    alert("nope");
-  }
-});*/
+    if (playerTiles.indexOf(winningCombinations[i][0]) >= 0 &&
+   playerTiles.indexOf(winningCombinations[i][1]) >= 0 &&
+    playerTiles.indexOf(winningCombinations[i][2]) >= 0){
 
+  var win = confirm(playerTurn+"wins!\n Play again?");
+  if (win == true) {
+      location.reload();
+  } else {
+      alert("Good game. \n Remember to shake hands.");
+  }
+    }
+    }
+    if (tilesClicked.length == 9){
+      var tie = confirm("Tie Game!\n Play again?");
+      if (tie == true) {
+          location.reload();
+      } else {
+          alert("Good game. \n Remember to shake hands.");
+      }
+    }
+  };
+
+var cpuTurn = function(cpuSide){
+  if(cpuSide == "X"){playerTurn = "O";}
+  else {playerTurn = "X";}
+};
 
 });
